c3e9e3e3d7915aaadf1d6dee72b45fddeb995baa
andy-smoker <mralan1407@gmail.com>
2021-08-10 22:45:07 +0300

    some code

--- clerk.go
+++ clerk.go
@@ -2,46 +2,63 @@ package clerk
 
 import (
 	"fmt"
+	"os"
 	"sync"
+	"time"
 )
+
+var levels []string = []string{"DEBUG", "INFO", "ERROR", "WARNING"}
+var mods []string = []string{"trace", "info"}
 
 type Clerk struct {
 	*sync.WaitGroup
-	Printers []*Printer
+	Printers map[string]Printer
 }
 
 type Printer struct {
 	*sync.Mutex
 	Mode     string
 	FilePath string
-	Input    chan string
+}
+
+type Message struct {
+	Type string
+	Date time.Time
+	Log  string
 }
 
 func NewClerk() *Clerk {
-	return &Clerk{}
+	return &Clerk{
+		WaitGroup: &sync.WaitGroup{},
+		Printers:  map[string]Printer{},
+	}
 
 }
 
-func (c *Clerk) NewPrinter(mode, filepath string, input chan string) {
+func (c *Clerk) NewPrinter(mode, name, filepath string, input string) *Printer {
 	c.WaitGroup.Add(1)
 	p := Printer{
 		Mode:     "LOG",
-		FilePath: "./default.log",
-		Input:    input,
+		FilePath: "./" + name + ".log",
 	}
 	fmt.Sscan(mode, &p.Mode)
 	fmt.Sscan(filepath, &p.FilePath)
-	c.Printers = append(c.Printers, &p)
+	if _, err := os.Stat(p.FilePath); os.IsNotExist(err) {
+		os.Create(p.FilePath)
+	}
+	c.Printers[name] = p
+	return &p
 }
 
-func (c *Printer) ReadLog() {
-
-}
-
-func (c *Printer) WriteLog() {
-
-}
-
-func (c *Clerk) Start() {
-
+func (p *Printer) WriteLog(input string) {
+	file, err := os.Open(p.FilePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	p.Mutex.Lock()
+	_, err = file.WriteString(input)
+	p.Mutex.Unlock()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
